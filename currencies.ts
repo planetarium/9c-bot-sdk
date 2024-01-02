c338dbd6c865695475cad38f41201d5044163e7e
moreal <dev.moreal@gmail.com>
2024-01-02 17:13:15 +0900

    Support odin and heimdall NCG

--- currencies.ts
+++ currencies.ts
@@ -1,9 +1,17 @@
-import type { Currency } from "https://esm.sh/@planetarium/tx@3.9.2";
+import { Address, type Currency } from "./deps.ts";
 
-const MinterlessLegacyCurrency = {
-  minters: null,
+const LegacyCurrency = {
   maximumSupply: null,
   totalSupplyTrackable: false,
+};
+
+const MinterlessCurrency = {
+  minters: null,
+};
+
+const MinterlessLegacyCurrency = {
+  ...MinterlessCurrency,
+  ...LegacyCurrency,
 };
 
 export const MEAD: Currency = {
@@ -17,3 +25,27 @@ export const CRYSTAL: Currency = {
   decimalPlaces: 18,
   ...MinterlessLegacyCurrency,
 } as const;
+
+const NcgTemplate = {
+  ticker: "NCG",
+  decimalPlaces: 2,
+  ...LegacyCurrency,
+} as const;
+
+/**
+ * Create NCG currency with the given {@link minters}.
+ * @param minters An array of minter {@link Address | address}es.
+ * @returns The NCG currency with the given {@link minters}.
+ */
+export function NcgWithMinters(minters: Address[] | null): Currency {
+  return {
+    minters: minters === null ? null : new Set(minters.map((x) => x.toBytes())),
+    ...NcgTemplate,
+  };
+}
+
+export const ODIN_NCG: Currency = NcgWithMinters([
+  Address.fromHex("0x47d082a115c63e7b58b1532d20e631538eafadde", true),
+]);
+
+export const HEIMDALL_NCG: Currency = NcgWithMinters(null);
