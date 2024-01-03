3064ab5a23e2d2549103fa854045b2338cf3aa87
moreal <dev.moreal@gmail.com>
2024-01-03 09:55:45 +0900

    Move source code to `src` directory

--- mod.ts
+++ mod.ts
@@ -1,12 +1,12 @@
-export * from "./user/mod.ts";
-export * from "./signer.ts";
-export * from "./headless.ts";
-export * from "./actions.ts";
-export * from "./currencies.ts";
+export * from "./src/user/mod.ts";
+export * from "./src/signer.ts";
+export * from "./src/headless.ts";
+export * from "./src/actions.ts";
+export * from "./src/currencies.ts";
 export {
   Address,
   type Currency,
   type FungibleAssetValue,
   PublicKey,
   RawPrivateKey,
-} from "./deps.ts";
+} from "./src/deps.ts";
